c04d55058e329c67ed4f6368b1bd96925c6827e1
UandersonS <58696209+UandersonS@users.noreply.github.com>
2020-04-13 18:45:02 -0300

    Add files via upload

---
+++ Vetores/Vetores/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vetores
+{
+    class Produto
+    {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        
+    }
+}
---
+++ Vetores/Vetores/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Produto[] vect = new Produto[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Produto { Name = name, Price = price };
+            }
+            double soma = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                soma += vect[i].Price;    
+            }
+            double avg = soma / n;
+            Console.WriteLine($"AVERRAGE PRICE: {avg}");
+        }
+    }
+}
