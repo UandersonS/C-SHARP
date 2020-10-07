b326b205510ba361cb2aec5cfe5ede9327ab56cb
UandersonS <58696209+UandersonS@users.noreply.github.com>
2020-10-06 23:19:33 -0300

    Add files via upload

---
+++ SistemaOdontológico/Controllers/Context/SistemaContext.cs
@@ -0,0 +1,28 @@
+﻿using Controllers.Map;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers.Context
+{
+    class SistemaContext : DbContext
+    {
+        public SistemaContext() : base("Server=DESKTOP-84MRSJ9\\SQLEXPRESS; Database=BD_Odonto; Integrated Security= True;")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<SistemaContext>(null);
+            modelBuilder.Configurations.Add(new DentistaMap());
+            modelBuilder.Configurations.Add(new PacienteMap());
+            modelBuilder.Configurations.Add(new ConsultaMap());
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ SistemaOdontológico/Controllers/Map/DentistaMap.cs
@@ -0,0 +1,27 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers.Map
+{
+    class DentistaMap : EntityTypeConfiguration<Dentista>
+    {
+        public DentistaMap()
+        {
+            this.ToTable("Dentista");
+            this.HasKey(d => d.Id);
+            this.Property(d => d.Id).HasColumnName("ID_DENTISTA");
+            this.Property(d => d.Name).HasColumnName("Nome_Dentista");
+            this.Property(d => d.Email).HasColumnName("Email_Dentista");
+            this.Property(d => d.Telefone).HasColumnName("Telefone_Dentista");
+            this.Property(d => d.Celular).HasColumnName("Celular_Dentista");
+            this.Property(d => d.CRO).HasColumnName("CRO");
+
+        }
+    }
+}
